03a2d84cbc887d33ee53eb0e6feefff8670cfefe
TheHawk3r <cin5marian@gmail.com>
2021-11-12 15:42:42 +0200

    Minimal functionality implemented for "CanHaveASingleDigit" test to pass.

--- Json.Start/Json.Validator/JsonNumber.cs
+++ Json.Start/Json.Validator/JsonNumber.cs
@@ -4,6 +4,9 @@ namespace Json
 {
     public static class JsonNumber
     {
+        private const int AsciiDigitRangeMin = 47;
+        private const int AsciiDigitRangeMax = 58;
+
         public static bool IsJsonNumber(string input)
         {
             if (input == null)
@@ -11,7 +14,7 @@ namespace Json
                 return false;
             }
 
-            return input.Contains('0');
+            return input[0] > AsciiDigitRangeMin && input[0] < AsciiDigitRangeMax;
         }
     }
 }
