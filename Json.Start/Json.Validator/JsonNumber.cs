edc5ce720ec66824e22e069683ebf75c7f1044ae
TheHawk3r <cin5marian@gmail.com>
2021-11-12 16:12:58 +0200

    Minimal functionality implemented for "IsNotAnEmptString" test to pass

--- Json.Start/Json.Validator/JsonNumber.cs
+++ Json.Start/Json.Validator/JsonNumber.cs
@@ -14,6 +14,11 @@ namespace Json
                 return false;
             }
 
+            if (input == string.Empty)
+            {
+                return false;
+            }
+
             for (int i = 0; i < input.Length; i++)
             {
                 if (input[i] < AsciiDigitRangeMin || input[i] > AsciiDigitRangeMax)
