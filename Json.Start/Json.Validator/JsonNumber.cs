67b850085fa1a96be06a0f7388e17fea05c6fec0
TheHawk3r <cin5marian@gmail.com>
2021-11-12 15:34:48 +0200

    Minimal functionality implemented for "DoesNotContainLetters" test to pass

--- Json.Start/Json.Validator/JsonNumber.cs
+++ Json.Start/Json.Validator/JsonNumber.cs
@@ -6,7 +6,12 @@ namespace Json
     {
         public static bool IsJsonNumber(string input)
         {
-            return true;
+            if (input == null)
+            {
+                return false;
+            }
+
+            return input.Contains('0');
         }
     }
 }
