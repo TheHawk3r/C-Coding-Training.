512389dc7e4b6a1ae211ec3def03d75a305b56fc
TheHawk3r <cin5marian@gmail.com>
2021-11-19 14:03:34 +0200

    Removed redundant code.

--- Json.Start/Json.Validator/JsonNumber.cs
+++ Json.Start/Json.Validator/JsonNumber.cs
@@ -4,8 +4,6 @@ namespace Json
 {
     public static class JsonNumber
     {
-        private const int AsciiDigitRangeMin = 47;
-        private const int AsciiDigitRangeMax = 58;
         private const int Two = 2;
 
         public static bool IsJsonNumber(string input)
@@ -148,120 +146,6 @@ namespace Json
         static bool ExponentSignIsValid(char sign)
         {
             return char.IsDigit(sign) || sign == '-' || sign == '+';
-        }
-
-        static bool NumberExponentIsAfterFraction(string input, bool hasFraction, bool hasExponent)
-        {
-            if (!hasFraction || !hasExponent)
-            {
-                return true;
-            }
-
-            bool firstGroupOfConditions =
-               !NumberEndsWithADot(input)
-               && !NumberStartsWithADot(input)
-               && !NumberHasMultipleExponents(input)
-               && NumberExponentIsComplete(input);
-
-            bool secondGropuOfConditions =
-                FractionCanHaveLeadingZeros(input)
-                && !NumberHasMultipleFractionParts(input)
-                && NumberExponentIsAfterFraction(input, input.Contains('.'), input.Contains('e') || input.Contains('E'))
-                && CheckNumberCharactersAreValid(input);
-
-            input = input.ToLower();
-            int indexOfFraction = input.IndexOf('.');
-            int indexOfExponent = input.IndexOf('e');
-
-            return indexOfExponent > indexOfFraction;
-        }
-
-        static bool NumberHasTooManyPlusesOrMinuses(string input)
-        {
-            int countOfPluses = input.Length - input.Replace("+", "").Length;
-            if (input[0] == '-')
-            {
-                input = input.Remove(0, 1);
-            }
-
-            int countOfMinuses = input.Length - input.Replace("-", "").Length;
-
-            return countOfMinuses > 1 || countOfPluses > 1;
-        }
-
-        static bool NumberExponentIsComplete(string input)
-        {
-            input = input.ToLower();
-            int indexOfExponent = input.IndexOf('e');
-            if (indexOfExponent == input.Length - 1)
-            {
-                return false;
-            }
-
-            if ((input[indexOfExponent + 1] == '+' || input[indexOfExponent + 1] == '-') && input.Length - 1 == indexOfExponent + 1)
-            {
-                return false;
-            }
-
-            return !NumberHasTooManyPlusesOrMinuses(input);
-        }
-
-        static bool NumberHasMultipleFractionParts(string input)
-        {
-            int countOfDots = input.Length - input.Replace(".", "").Length;
-
-            return countOfDots > 1;
-        }
-
-        static bool NumberHasMultipleExponents(string input)
-        {
-            input = input.ToLower();
-            int countOfExponents = input.Length - input.Replace("e", "").Length;
-
-            return countOfExponents > 1;
-        }
-
-        static bool NumberEndsWithADot(string input)
-        {
-            return input[input.Length - 1] == '.';
-        }
-
-        static bool NumberStartsWithZero(string input)
-        {
-            return input.Length > 1 && input[0] == '0';
-        }
-
-        static bool FractionCanHaveLeadingZeros(string input)
-        {
-            return !(NumberStartsWithZero(input) && !input.Contains('.'));
-        }
-
-        static bool NumberStartsWithADot(string input)
-        {
-            return input.Length > 1 && input[0] == '.';
-        }
-
-        static bool CheckNumberCharactersAreValid(string input)
-        {
-            for (int i = input[0] == '-' ? 1 : 0; i < input.Length; i++)
-            {
-                if (input[i] == '.' || input[i] == 'e' || input[i] == 'E')
-                {
-                    continue;
-                }
-
-                if (input[i] == '-' || input[i] == '+')
-                {
-                    continue;
-                }
-
-                if (input[i] < AsciiDigitRangeMin || input[i] > AsciiDigitRangeMax)
-                {
-                    return false;
-                }
-            }
-
-            return true;
         }
     }
 }
