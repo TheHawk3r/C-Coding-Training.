a27b7d89e468410fea26c3aea7dc618ae9fc97c8
TheHawk3r <cin5marian@gmail.com>
2021-11-22 19:42:34 +0200

    Added functionality to add team and find a team based on its position within the ranking.

--- SoccerRanking/SoccerRanking/Ranking.cs
+++ SoccerRanking/SoccerRanking/Ranking.cs
@@ -1,3 +1,24 @@
-﻿namespace SoccerRanking
+﻿using System.Collections.Generic;
+
+namespace SoccerRanking
 {
+    class Ranking
+    {
+        readonly List<SoccerTeam> teams;
+
+        public Ranking(List<SoccerTeam> teams)
+        {
+            this.teams = teams;
+        }
+
+        public void AddTeam(SoccerTeam team)
+        {
+            teams.Add(team);
+        }
+
+        public string GetTeamOnPosition(int position)
+        {
+            return teams[position].GetName();
+        }
+    }
 }
