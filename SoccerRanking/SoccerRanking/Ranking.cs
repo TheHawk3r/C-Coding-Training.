f3b95b0dfef3e938e3bba9f67ecad3a145695139
TheHawk3r <cin5marian@gmail.com>
2021-11-24 16:18:12 +0200

    Added tests For "UpdateRankingBasedOnMatch" method and fixed bugs.

--- SoccerRanking/SoccerRanking/Ranking.cs
+++ SoccerRanking/SoccerRanking/Ranking.cs
@@ -37,15 +37,21 @@ namespace SoccerRanking
         {
             int firstTeamIndex = teams.FindIndex(x => x.GetName().Contains(firstTeamName));
             int secondTeamIndex = teams.FindIndex(x => x.GetName().Contains(secondTeamName));
+            string[] matchResultArray = matchResult.Split('-');
+
+            if (firstTeamIndex == -1 || secondTeamIndex == -1 || matchResultArray.Length != 2)
+            {
+                return;
+            }
+
             int firstTeamWonMatches = teams[firstTeamIndex].GetWonMatches();
             int firstTeamLostMatches = teams[firstTeamIndex].GetLostMatches();
             int firstTeamDrawMatches = teams[firstTeamIndex].GetDraws();
             int secondTeamWonMatches = teams[secondTeamIndex].GetWonMatches();
             int secondTeamLostMatches = teams[secondTeamIndex].GetLostMatches();
             int secondTeamDrawMatches = teams[secondTeamIndex].GetDraws();
-            string[] matchResultArray = matchResult.Split('-');
 
-            if (int.TryParse(matchResultArray[0], out int firstTeamScore) && int.TryParse(matchResultArray[1], out int secondTeamScore) && firstTeamIndex != -1 && secondTeamIndex != -1)
+            if (int.TryParse(matchResultArray[0], out int firstTeamScore) && int.TryParse(matchResultArray[1], out int secondTeamScore))
             {
                 if (firstTeamScore > secondTeamScore)
                 {
--- SoccerRanking/SoccerRanking/RankingFacts.cs
+++ SoccerRanking/SoccerRanking/RankingFacts.cs
@@ -47,6 +47,30 @@ namespace SoccerRanking
         }
 
         [Fact]
+        public void RankingGetTeamOnPositionShouldReturnTheTeamOnTheSpecifiedPosition()
+        {
+            List<SoccerTeam> soccerTeams = new List<SoccerTeam>();
+            soccerTeams.Add(new SoccerTeam("CFR Cluj", 10, 4, 5));
+            soccerTeams.Add(new SoccerTeam("FCSB", 7, 4, 2));
+            soccerTeams.Add(new SoccerTeam("Univ. Craiova", 5, 7, 10));
+            Ranking ranking = new Ranking(soccerTeams);
+
+            Assert.Equal("FCSB", ranking.GetTeamOnPosition(3));
+        }
+
+        [Fact]
+        public void RankingGetPositionOfTeamShouldReturnThePositionOfSpecifiedTeam()
+        {
+            List<SoccerTeam> soccerTeams = new List<SoccerTeam>();
+            soccerTeams.Add(new SoccerTeam("CFR Cluj", 10, 4, 5));
+            soccerTeams.Add(new SoccerTeam("FCSB", 7, 4, 2));
+            soccerTeams.Add(new SoccerTeam("Univ. Craiova", 5, 7, 10));
+            Ranking ranking = new Ranking(soccerTeams);
+
+            Assert.Equal(3, ranking.GetPositionOfTeam("FCSB"));
+        }
+
+        [Fact]
         public void RankingUpdatesCorrectlyAfterMatch()
         {
             List<SoccerTeam> soccerTeams = new List<SoccerTeam>();
@@ -63,7 +87,7 @@ namespace SoccerRanking
         }
 
         [Fact]
-        public void RankingGetTeamOnPositionShouldReturnTheTeamOnTheSpecifiedPosition()
+        public void RankingUpdateRankingBasedOnMatchIfNameOfAnyTeamIsNotValidShouldNotMakeAnyChanges()
         {
             List<SoccerTeam> soccerTeams = new List<SoccerTeam>();
             soccerTeams.Add(new SoccerTeam("CFR Cluj", 10, 4, 5));
@@ -71,7 +95,30 @@ namespace SoccerRanking
             soccerTeams.Add(new SoccerTeam("Univ. Craiova", 5, 7, 10));
             Ranking ranking = new Ranking(soccerTeams);
 
-            Assert.Equal("FCSB", ranking.GetTeamOnPosition(3));
+            ranking.UpdateRankingBasedOnMatch("CFR Clj", "2-1", "Uiv. Craiova");
+            ranking.UpdateRankingBasedOnMatch("Dinamo Bucuresti", "3-2", "FCSB");
+
+            Assert.Equal(35, ranking.GetTeams()[0].GetPoints());
+            Assert.Equal(10, ranking.GetTeams()[0].GetWonMatches());
+            Assert.Equal(7, ranking.GetTeams()[1].GetLostMatches());
+        }
+
+        [Fact]
+        public void RankingUpdateRankingBasedOnMatchIfScoreIsNotValidShouldNotMakeAnyChanges()
+        {
+            List<SoccerTeam> soccerTeams = new List<SoccerTeam>();
+            soccerTeams.Add(new SoccerTeam("CFR Cluj", 10, 4, 5));
+            soccerTeams.Add(new SoccerTeam("FCSB", 7, 4, 2));
+            soccerTeams.Add(new SoccerTeam("Univ. Craiova", 5, 7, 10));
+            Ranking ranking = new Ranking(soccerTeams);
+
+            ranking.UpdateRankingBasedOnMatch("CFR Cluj", "21", "Univ. Craiova");
+            ranking.UpdateRankingBasedOnMatch("CFR Cluj", "=-=", "Univ. Craiova");
+            ranking.UpdateRankingBasedOnMatch("CFR Cluj", "1,2", "Univ. Craiova");
+
+            Assert.Equal(35, ranking.GetTeams()[0].GetPoints());
+            Assert.Equal(10, ranking.GetTeams()[0].GetWonMatches());
+            Assert.Equal(7, ranking.GetTeams()[1].GetLostMatches());
         }
     }
 }
