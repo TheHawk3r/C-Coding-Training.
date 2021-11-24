470339a50535024341cd05edb7a14caabe08d63e
TheHawk3r <cin5marian@gmail.com>
2021-11-24 14:18:50 +0200

    Implemented "UpdateRankingBasedOnMatch" , "GetTeams" methods and added two tests in RankingFacts.cs

--- SoccerRanking/SoccerRanking/Ranking.cs
+++ SoccerRanking/SoccerRanking/Ranking.cs
@@ -12,6 +12,11 @@ namespace SoccerRanking
             teams.Sort();
         }
 
+        public List<SoccerTeam> GetTeams()
+        {
+            return this.teams;
+        }
+
         public void AddTeam(SoccerTeam team)
         {
             teams.Add(team);
@@ -26,6 +31,48 @@ namespace SoccerRanking
         public int GetPositionOfTeam(string nameOfTeam)
         {
             return teams.FindIndex(x => x.GetName().Contains(nameOfTeam)) + 1;
+        }
+
+        public void UpdateRankingBasedOnMatch(string firstTeamName, string matchResult, string secondTeamName)
+        {
+            int firstTeamIndex = teams.FindIndex(x => x.GetName().Contains(firstTeamName));
+            int secondTeamIndex = teams.FindIndex(x => x.GetName().Contains(secondTeamName));
+            int firstTeamWonMatches = teams[firstTeamIndex].GetWonMatches();
+            int firstTeamLostMatches = teams[firstTeamIndex].GetLostMatches();
+            int firstTeamDrawMatches = teams[firstTeamIndex].GetDraws();
+            int secondTeamWonMatches = teams[secondTeamIndex].GetWonMatches();
+            int secondTeamLostMatches = teams[secondTeamIndex].GetLostMatches();
+            int secondTeamDrawMatches = teams[secondTeamIndex].GetDraws();
+            string[] matchResultArray = matchResult.Split('-');
+
+            if (int.TryParse(matchResultArray[0], out int firstTeamScore) && int.TryParse(matchResultArray[1], out int secondTeamScore) && firstTeamIndex != -1 && secondTeamIndex != -1)
+            {
+                if (firstTeamScore > secondTeamScore)
+                {
+                    firstTeamWonMatches++;
+                    secondTeamLostMatches++;
+                }
+                else if (firstTeamScore == secondTeamScore)
+                {
+                    firstTeamDrawMatches++;
+                    secondTeamDrawMatches++;
+                }
+                else
+                {
+                    secondTeamWonMatches++;
+                    firstTeamLostMatches--;
+                }
+            }
+            else
+            {
+                return;
+            }
+
+            teams.RemoveAt(firstTeamIndex);
+            teams.RemoveAt(secondTeamIndex);
+            teams.Add(new SoccerTeam(firstTeamName, firstTeamWonMatches, firstTeamLostMatches, firstTeamDrawMatches));
+            teams.Add(new SoccerTeam(secondTeamName, secondTeamWonMatches, secondTeamLostMatches, secondTeamDrawMatches));
+            teams.Sort();
         }
     }
 }
--- SoccerRanking/SoccerRanking/RankingFacts.cs
+++ SoccerRanking/SoccerRanking/RankingFacts.cs
@@ -18,5 +18,31 @@ namespace SoccerRanking
             Assert.Equal(35, soccerTeams[0].GetPoints());
             Assert.Equal(25, soccerTeams[1].GetPoints());
         }
+
+        [Fact]
+        public void RankingGetTeamsMethodReturnsTeamsInRanking()
+        {
+            List<SoccerTeam> soccerTeams = new List<SoccerTeam>();
+            soccerTeams.Add(new SoccerTeam("CFR Cluj", 10, 4, 5));
+            soccerTeams.Add(new SoccerTeam("FCSB", 7, 4, 2));
+            soccerTeams.Add(new SoccerTeam("Univ. Craiova", 5, 7, 10));
+            Ranking ranking = new Ranking(soccerTeams);
+
+            Assert.Equal(soccerTeams, ranking.GetTeams());
+        }
+
+        [Fact]
+        public void RankingUpdatesCorrectlyAfterMatch()
+        {
+            List<SoccerTeam> soccerTeams = new List<SoccerTeam>();
+            soccerTeams.Add(new SoccerTeam("CFR Cluj", 10, 4, 5));
+            soccerTeams.Add(new SoccerTeam("FCSB", 7, 4, 2));
+            soccerTeams.Add(new SoccerTeam("Univ. Craiova", 5, 7, 10));
+            Ranking ranking = new Ranking(soccerTeams);
+
+            ranking.UpdateRankingBasedOnMatch("CFR Cluj", "2-1", "Univ. Craiova");
+
+            Assert.Equal(38, ranking.GetTeams()[0].GetPoints());
+        }
     }
 }
