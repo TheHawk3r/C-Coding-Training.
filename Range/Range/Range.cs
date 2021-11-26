a8967a5bda79c3d804f8fd41644b424050f155f9
TheHawk3r <cin5marian@gmail.com>
2021-11-26 15:48:56 +0200

    Added tests for the Range class.

--- Range/Range/Range.cs
+++ Range/Range/Range.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace Range
+﻿namespace Range
 {
     class Range
     {
--- Range/Range/RangeFacts.cs
+++ Range/Range/RangeFacts.cs
@@ -5,11 +5,67 @@ namespace Range
     public class RangeFacts
     {
         [Fact]
-        public void RangeMatch()
+        public void RangeMatchMethodIfTextIsNullReturnsFalse()
+        {
+            var digit = new Range('a', 'f');
+
+            Assert.False(digit.Match(null));
+        }
+
+        [Fact]
+        public void RangeMatchMethodIfTextIsEmptyReturnsFalse()
+        {
+            var digit = new Range('a', 'f');
+
+            Assert.False(digit.Match(string.Empty));
+        }
+
+        [Fact]
+        public void RangeMatchMethodTextThatIsInRangeOfCharactersShouldReturnTrue()
+        {
+            var digit = new Range('1', '4');
+
+            Assert.True(digit.Match("123"));
+        }
+
+        [Fact]
+        public void RangeMatchMethodTextThatIsNotInRangeOfCharactersShouldReturnTrue()
+        {
+            var digit = new Range('7', '9');
+
+            Assert.False(digit.Match("34"));
+        }
+
+        [Fact]
+        public void RangeMatchMethodHomeWorkFirstStringShouldReturnTrue()
         {
             var digit = new Range('a', 'f');
 
             Assert.True(digit.Match("abc"));
+        }
+
+        [Fact]
+        public void RangeMatchMethodHomeWorkSecondStringShouldReturnTrue()
+        {
+            var digit = new Range('a', 'f');
+
+            Assert.True(digit.Match("fab"));
+        }
+
+        [Fact]
+        public void RangeMatchMethodHomeWorkThirdStringShouldReturnTrue()
+        {
+            var digit = new Range('a', 'f');
+
+            Assert.True(digit.Match("bcd"));
+        }
+
+        [Fact]
+        public void RangeMatchMethodHomeWorkFourthStringShouldReturnFalse()
+        {
+            var digit = new Range('a', 'f');
+
+            Assert.False(digit.Match("1ab"));
         }
     }
 }
