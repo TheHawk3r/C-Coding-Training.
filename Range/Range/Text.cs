eeaaf83b27585e45d713e7e74710f368034b82d2
TheHawk3r <cin5marian@gmail.com>
2021-12-06 16:10:03 +0200

    Completed Text Class.

---
+++ Range/Range/Text.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Range
+{
+    class Text : IPattern
+    {
+        readonly string prefix;
+
+        public Text(string prefix)
+        {
+            this.prefix = prefix;
+        }
+
+        public IMatch Match(string text)
+        {
+            return text?.StartsWith(this.prefix) == true
+            ? new Match(text[this.prefix.Length..], true)
+            : new Match(text, false);
+        }
+    }
+}
---
+++ Range/Range/TextTheories.cs
@@ -0,0 +1,74 @@
+﻿using Xunit;
+
+namespace Range
+{
+    public class TextTheories
+    {
+        [Theory]
+        [InlineData("true", "")]
+        [InlineData("trueX", "X")]
+        public void ReturnsAValidMatchWhenTheTextStartsWithThePrefixTrue(string text, string remainingText)
+        {
+            var t = new Text("true");
+
+            Assert.True(t.Match(text).Success());
+            Assert.Equal(remainingText, t.Match(text).RemainingText());
+        }
+
+        [Theory]
+        [InlineData("false", "false")]
+        [InlineData("", "")]
+        [InlineData(null, null)]
+        public void ReturnsAnInvalidMatchWhenTheTextDoesNotStartWithThePrefixTrue(string text, string remainingText)
+        {
+            var t = new Text("true");
+
+            Assert.False(t.Match(text).Success());
+            Assert.Equal(remainingText, t.Match(text).RemainingText());
+        }
+
+        [Theory]
+        [InlineData("false", "")]
+        [InlineData("falseX", "X")]
+        public void ReturnsAValidMatchWhenTheTextStartsWithThePrefixFalse(string text, string remainingText)
+        {
+            var f = new Text("false");
+
+            Assert.True(f.Match(text).Success());
+            Assert.Equal(remainingText, f.Match(text).RemainingText());
+        }
+
+        [Theory]
+        [InlineData("true", "true")]
+        [InlineData("", "")]
+        [InlineData(null, null)]
+        public void ReturnsAnInvalidMatchWhenTheTextDoesNotStartWithTHePrefixFalse(string text, string remainingText)
+        {
+            var f = new Text("false");
+
+            Assert.False(f.Match(text).Success());
+            Assert.Equal(remainingText, f.Match(text).RemainingText());
+        }
+
+        [Theory]
+        [InlineData("true", "true")]
+        [InlineData("false", "false")]
+        public void ReturnsAValidMatchIfThePrefixIsEmpty(string text, string remainingText)
+        {
+            var empty = new Text("");
+
+            Assert.True(empty.Match(text).Success());
+            Assert.Equal(remainingText, empty.Match(text).RemainingText());
+        }
+
+        [Theory]
+        [InlineData(null, null)]
+        public void ReturnsAnInvalidMatchIfThePrefixIsEmptyAndTheTextisNull(string text, string remainingText)
+        {
+            var empty = new Text("");
+
+            Assert.False(empty.Match(text).Success());
+            Assert.Equal(remainingText, empty.Match(text).RemainingText());
+        }
+    }
+}
