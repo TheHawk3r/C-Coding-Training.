78ab7f06ec5380833d281cf4b8650c06cc443517
TheHawk3r <cin5marian@gmail.com>
2021-12-06 16:24:21 +0200

    Added new InlineData.

--- Range/Range/TextTheories.cs
+++ Range/Range/TextTheories.cs
@@ -16,6 +16,7 @@ namespace Range
         }
 
         [Theory]
+        [InlineData("Xtrue", "Xtrue")]
         [InlineData("false", "false")]
         [InlineData("", "")]
         [InlineData(null, null)]
@@ -39,6 +40,7 @@ namespace Range
         }
 
         [Theory]
+        [InlineData("Xfalse", "Xfalse")]
         [InlineData("true", "true")]
         [InlineData("", "")]
         [InlineData(null, null)]
