fe2495acc666562f8041f221b7ff17fba664cb9b
TheHawk3r <cin5marian@gmail.com>
2021-12-08 16:58:07 +0200

    Completed OneOrMore class.

---
+++ Range/Range/OneOrMore.cs
@@ -0,0 +1,19 @@
+﻿namespace Range
+{
+    class OneOrMore : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public OneOrMore(IPattern pattern)
+        {
+            this.pattern = new Many(pattern);
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text).RemainingText() == text
+                ? new Match(text, false)
+                : pattern.Match(text);
+        }
+    }
+}
---
+++ Range/Range/OneOrMoreTheories.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+
+namespace Range
+{
+    public class OneOrMoreTheories
+    {
+        [Theory]
+        [InlineData("123", "")]
+        [InlineData("1a", "a")]
+        public void Test1(string text, string remainingText)
+        {
+            var a = new OneOrMore(new Range('0', '9'));
+
+            Assert.True(a.Match(text).Success());
+            Assert.Equal(remainingText, a.Match(text).RemainingText());
+        }
+
+        [Theory]
+        [InlineData("bc", "bc")]
+        [InlineData("", "")]
+        [InlineData(null, null)]
+        public void Test2(string text, string remainingText)
+        {
+            var a = new OneOrMore(new Range('0', '9'));
+
+            Assert.False(a.Match(text).Success());
+            Assert.Equal(remainingText, a.Match(text).RemainingText());
+        }
+    }
+}
