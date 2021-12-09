7369a59c97180fcc776bf32c4d6e7b199f985df7
TheHawk3r <cin5marian@gmail.com>
2021-12-09 17:04:20 +0200

    Implemented List Class partially.

--- Range/Range/List.cs
+++ Range/Range/List.cs
@@ -1,3 +1,17 @@
 ﻿namespace Range
 {
+    class List : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public List(IPattern element, IPattern separator)
+        {
+            this.pattern = new Sequence(new Optional(element), new Optional(separator), new Optional(element), new Optional(separator), new Optional(element));
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
 }
--- Range/Range/ListTheories.cs
+++ Range/Range/ListTheories.cs
@@ -1,1 +1,37 @@
-﻿
+﻿using Xunit;
+
+namespace Range
+{
+    public class ListTheories
+    {
+        [Theory]
+        [InlineData("1,2,3", "")]
+        [InlineData("1,2,3,", ",")]
+        [InlineData("1a", "a")]
+        [InlineData("abc", "abc")]
+        [InlineData("", "")]
+        [InlineData(null, null)]
+        public void Test1(string text, string remainingText)
+        {
+            var a = new List(new Range('0', '9'), new Character(','));
+
+            Assert.True(a.Match(text).Success());
+            Assert.Equal(remainingText, a.Match(text).RemainingText());
+        }
+
+        [Theory]
+        [InlineData("1; 22  ;\n 333 \t; 22", "")]
+        [InlineData("1 \n;", " \n;")]
+        [InlineData("abc", "abc")]
+        public void Test2(string text, string remainingText)
+        {
+            var digits = new OneOrMore(new Range('0', '9'));
+            var whitespace = new Many(new Any(" \r\n\t"));
+            var separator = new Sequence(whitespace, new Character(';'), whitespace);
+            var list = new List(digits, separator);
+
+            Assert.True(list.Match(text).Success());
+            Assert.Equal(remainingText, list.Match(text).RemainingText());
+        }
+    }
+}
