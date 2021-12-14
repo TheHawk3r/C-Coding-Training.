12560877ddf7d1b8c3ec1d4a88e354acbd543d60
TheHawk3r <cin5marian@gmail.com>
2021-12-14 17:59:07 +0200

    Started implementing Number class.

--- Range/Range/Number.cs
+++ Range/Range/Number.cs
@@ -6,9 +6,34 @@
 
         public Number()
         {
+            Range oneNine = new Range('1', '9');
+            Range digit = new Range('0', '9');
+            Choice exponentChoice = new Choice(new Character('e'), new Character('E'));
+            var digitNumber = new Sequence(new Optional(new Character('-')), digit);
+            var digitsFractionalNumber = new Sequence(
+                new Optional(new Character('-')), new OneOrMore(oneNine), new Many(digit), new Character('.'), new OneOrMore(digit));
+            var digitFractionalNumber = new Sequence(
+                    new Optional(new Character('-')), new Character('0'), new Character('.'), new OneOrMore(digit));
+            var digitsNumber = new Sequence(
+                    new Optional(new Character('-')), new OneOrMore(oneNine), new Many(digit));
+            var digitsNumberDigitExponent = new Sequence(
+                    new Optional(new Character('-')), new OneOrMore(oneNine), new Many(digit), exponentChoice, new Character('0'));
+            var digitsNumberDigitsExponent = new Sequence(
+                    new Optional(new Character('-')), new OneOrMore(oneNine), new Many(digit), exponentChoice, new OneOrMore(oneNine), new Many(digit));
+            var digitNumberDigitExponent = new Sequence(
+                    new Optional(new Character('-')), digit, exponentChoice, new Character('0'));
+            var digitNumberDigitsExponent = new Sequence(
+                    new Optional(new Character('-')), digit, exponentChoice, new OneOrMore(oneNine), new Many(digit));
+
             pattern = new Choice(
-                new Sequence(new Optional(new Character('-')), new OneOrMore(new Range('1', '9')), new Many(new Range('0', '9'))),
-                new Sequence(new Optional(new Character('-')), new Range('0', '9')));
+                digitsFractionalNumber,
+                digitFractionalNumber,
+                digitsNumber,
+                digitsNumberDigitExponent,
+                digitsNumberDigitsExponent,
+                digitNumber,
+                digitNumberDigitExponent,
+                digitNumberDigitsExponent);
         }
 
         public IMatch Match(string text)
--- Range/Range/NumberTheories.cs
+++ Range/Range/NumberTheories.cs
@@ -6,9 +6,17 @@ namespace Range
     {
         [Theory]
         [InlineData("0", "")]
+        [InlineData("-0", "")]
         [InlineData("7", "")]
         [InlineData("70", "")]
-        [InlineData("07", "7")]
+        [InlineData("-25", "")]
+        [InlineData("12.34", "")]
+        [InlineData("0.00000001", "")]
+        [InlineData("10.000000001", "")]
+        [InlineData("12e3", "")]
+        [InlineData("123.", ".")]
+        [InlineData("12.34.56", ".56")]
+        [InlineData("12.3x", "x")]
         public void IsNumber(string text, string remainingText)
         {
             var number = new Number();
@@ -17,8 +25,18 @@ namespace Range
             Assert.Equal(remainingText, number.Match(text).RemainingText());
         }
 
+        [Fact]
+        public void ReturnsAValidMatchForANumberThatBegginsWithZeroAndOnlyTheFirstDIgitIsConsumed()
+        {
+            var number = new Number();
+
+            Assert.True(number.Match("07").Success());
+            Assert.Equal("7", number.Match("07").RemainingText());
+        }
+
         [Theory]
         [InlineData("a", "a")]
+        [InlineData(".153", ".153")]
         [InlineData("", "")]
         [InlineData(null, null)]
 
