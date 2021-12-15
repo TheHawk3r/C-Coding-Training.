5b8fa69cb07cb50ea31140b7d01a3e544c7d2617
TheHawk3r <cin5marian@gmail.com>
2021-12-15 13:56:52 +0200

    Refactored code.

--- Range/Range/Number.cs
+++ Range/Range/Number.cs
@@ -9,11 +9,10 @@
             Range oneNine = new Range('1', '9');
             Range digit = new Range('0', '9');
 
-            var integer = new Choice(new Sequence(new Character('-'), oneNine, new Many(digit)), new Sequence(new Character('-'), digit), new Sequence(oneNine, new Many(digit)), new Sequence(digit));
-            var fraction = new Sequence(new Character('.'), new OneOrMore(digit));
-            var exponent = new Choice(
-                new Sequence(new Character('E'), new Optional(new Choice(new Character('+'), new Character('-'))), new OneOrMore(digit)),
-                new Sequence(new Character('e'), new Optional(new Choice(new Character('+'), new Character('-'))), new OneOrMore(digit)));
+            var digits = new OneOrMore(digit);
+            var integer = new Choice(new Sequence(new Character('-'), oneNine, digits), new Sequence(new Character('-'), digit), new Sequence(oneNine, digits), digit);
+            var fraction = new Sequence(new Character('.'), digits);
+            var exponent = new Sequence(new Any("eE"), new Optional(new Any("+-")), digits);
 
             pattern = new Sequence(integer, new Optional(fraction), new Optional(exponent));
         }
