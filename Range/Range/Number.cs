96aeffc2c3f11a6bec38cf57d94b154aff331ab5
TheHawk3r <cin5marian@gmail.com>
2021-12-15 14:10:22 +0200

    Refactored integer var.

--- Range/Range/Number.cs
+++ Range/Range/Number.cs
@@ -10,7 +10,7 @@
             Range digit = new Range('0', '9');
 
             var digits = new OneOrMore(digit);
-            var integer = new Choice(new Sequence(new Optional(new Character('-')), oneNine, digits), new Sequence(new Optional(new Character('-')), digit));
+            var integer = new Sequence(new Optional(new Character('-')), new Choice(new Sequence(oneNine, digits), digit));
             var fraction = new Sequence(new Character('.'), digits);
             var exponent = new Sequence(new Any("eE"), new Optional(new Any("+-")), digits);
 
