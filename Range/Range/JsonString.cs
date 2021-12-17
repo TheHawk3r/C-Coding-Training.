b20536bc2647280b05c591a119dbaea5a887587a
TheHawk3r <cin5marian@gmail.com>
2021-12-17 15:06:54 +0200

    BrokeChoice into multiple lines.

--- Range/Range/JsonString.cs
+++ Range/Range/JsonString.cs
@@ -9,7 +9,11 @@
             Range digit = new Range('0', '9');
             var hex = new Choice(digit, new Range('A', 'F'), new Range('a', 'f'));
             var escape = new Choice(new Any("\"\\/bfnrt"), new Sequence(new Character('u'), hex, hex, hex, hex));
-            var character = new Choice(new Range('\u0020', '\u0021'), new Range('\u0023', '\u005b'), new Range('\u005d', char.MaxValue), new Sequence(new Character('\\'), escape));
+            var character = new Choice(
+                new Range('\u0020', '\u0021'),
+                new Range('\u0023', '\u005b'),
+                new Range('\u005d', char.MaxValue),
+                new Sequence(new Character('\\'), escape));
             var characters = new Choice(new OneOrMore(character));
             pattern = new Sequence(new Character('\"'), characters, new Character('\"'));
         }
