f5f21327f14e6a6ed272a3f7d03579c8459772d9
TheHawk3r <cin5marian@gmail.com>
2022-01-03 14:48:30 +0200

    Refactored JsonValidator Program.cs

--- JsonValidator/JsonValidator/Program.cs
+++ JsonValidator/JsonValidator/Program.cs
@@ -6,10 +6,17 @@ namespace JsonValidator
     class Program
     {
         static void Main(string[] args)
-        {
+            {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please input the path to the file that needs to be validated.");
+                return;
+            }
+
             var jsonValue = new Value();
             string text = System.IO.File.ReadAllText(args[0]);
-            Console.WriteLine($"{jsonValue.Match(text).Success()}");
+
+            Console.WriteLine(jsonValue.Match(text).Success() && jsonValue.Match(text).RemainingText() == string.Empty);
         }
     }
 }
