032d812c0f461a1caed7bce7faa8e6d0c757f778
TheHawk3r <cin5marian@gmail.com>
2022-01-06 17:04:49 +0200

    Started a DataCollections Solution with a class library, test projects and implemented first  intArray class.

---
+++ DataCollections/DataCollection.Tests/IntArrayTests.cs
@@ -0,0 +1,245 @@
+using System;
+using DataCollections;
+using Xunit;
+
+namespace DataCollection.Tests
+{
+    public class IntArrayTests
+    {
+        [Theory]
+        [InlineData(5)]
+        [InlineData(2)]
+        public void CanAddElements(int element)
+        {
+            var testArray = new IntArray();
+
+            testArray.Add(element);
+
+            Assert.Equal(element, testArray.Element(testArray.Count() - 1));
+        }
+
+        [Fact]
+        public void CanReturnCountOfAThreeIntegerArray()
+        {
+            var testArray = new IntArray();
+
+            testArray.Add(5);
+            testArray.Add(2);
+            testArray.Add(10);
+
+            Assert.Equal(3, testArray.Count());
+        }
+
+        [Fact]
+        public void CanReturnCountOfAFiveIntegerArray()
+        {
+            var testArray = new IntArray();
+
+            testArray.Add(5);
+            testArray.Add(2);
+            testArray.Add(10);
+            testArray.Add(4);
+            testArray.Add(0);
+
+            Assert.Equal(5, testArray.Count());
+        }
+
+        [Theory]
+        [InlineData(0, 5)]
+        [InlineData(1, 2)]
+        [InlineData(2, 10)]
+        [InlineData(3, 4)]
+        [InlineData(4, 0)]
+        public void CanReturnElementFromAGivenIndex(int index, int elementReturned)
+        {
+            var testArray = new IntArray();
+
+            testArray.Add(5);
+            testArray.Add(2);
+            testArray.Add(10);
+            testArray.Add(4);
+            testArray.Add(0);
+
+            Assert.Equal(elementReturned, testArray.Element(index));
+        }
+
+        [Theory]
+        [InlineData(0, 2)]
+        [InlineData(1, 1)]
+        [InlineData(2, 4)]
+        [InlineData(3, 8)]
+        [InlineData(4, 9)]
+        public void CanModifyElementFromAGivenIndex(int index, int elementToSet)
+        {
+            var testArray = new IntArray();
+
+            testArray.Add(5);
+            testArray.Add(2);
+            testArray.Add(10);
+            testArray.Add(4);
+            testArray.Add(0);
+
+            testArray.SetElement(index, elementToSet);
+
+            Assert.Equal(elementToSet, testArray.Element(index));
+        }
+
+        [Theory]
+        [InlineData(5)]
+        [InlineData(2)]
+        [InlineData(10)]
+        [InlineData(4)]
+        [InlineData(0)]
+        public void ShouldReturnTrueIfArrayContainsElement(int element)
+        {
+            var testArray = new IntArray();
+
+            testArray.Add(5);
+            testArray.Add(2);
+            testArray.Add(10);
+            testArray.Add(4);
+            testArray.Add(0);
+
+            Assert.True(testArray.Contains(element));
+        }
+
+        [Theory]
+        [InlineData(9)]
+        [InlineData(8)]
+        [InlineData(3)]
+        [InlineData(6)]
+        [InlineData(11)]
+        public void ShouldReturnFalseIfArrayDoesNotContainElement(int element)
+        {
+            var testArray = new IntArray();
+
+            testArray.Add(5);
+            testArray.Add(2);
+            testArray.Add(10);
+            testArray.Add(4);
+            testArray.Add(0);
+
+            Assert.False(testArray.Contains(element));
+        }
+
+        [Theory]
+        [InlineData(5, 0)]
+        [InlineData(2, 1)]
+        [InlineData(10, 2)]
+        [InlineData(4, 3)]
+        [InlineData(0, 4)]
+        public void ShouldReturnTheIndexOfAElementIfPresent(int element, int index)
+        {
+            var testArray = new IntArray();
+
+            testArray.Add(5);
+            testArray.Add(2);
+            testArray.Add(10);
+            testArray.Add(4);
+            testArray.Add(0);
+
+            Assert.Equal(index, testArray.IndexOf(element));
+        }
+
+        [Theory]
+        [InlineData(11)]
+        [InlineData(12)]
+        [InlineData(13)]
+        [InlineData(14)]
+        [InlineData(15)]
+        public void ShouldReturnMinusOneIfTheElementIsNotPresent(int element)
+        {
+            var testArray = new IntArray();
+
+            testArray.Add(5);
+            testArray.Add(2);
+            testArray.Add(10);
+            testArray.Add(4);
+            testArray.Add(0);
+
+            Assert.Equal(-1, testArray.IndexOf(element));
+        }
+
+        [Theory]
+        [InlineData(4, 0, 5)]
+        [InlineData(1, 1, 2)]
+        [InlineData(3, 2, 10)]
+        [InlineData(12, 3, 4)]
+        [InlineData(21, 4, 9)]
+        public void CanInsertElements(int element, int index, int nextElement)
+        {
+            var testArray = new IntArray();
+
+            testArray.Add(5);
+            testArray.Add(2);
+            testArray.Add(10);
+            testArray.Add(4);
+            testArray.Add(9);
+
+            testArray.Insert(index, element);
+
+            Assert.Equal(nextElement, testArray.Element(index + 1));
+            Assert.Equal(element, testArray.Element(index));
+        }
+
+        [Fact]
+        public void CanClearArray()
+        {
+            var testArray = new IntArray();
+
+            testArray.Add(5);
+            testArray.Add(2);
+            testArray.Add(10);
+            testArray.Add(4);
+            testArray.Add(9);
+
+            testArray.Clear();
+            Assert.Equal(0, testArray.Count());
+        }
+
+        [Theory]
+        [InlineData(5, 2)]
+        [InlineData(4, 3)]
+        [InlineData(10, 4)]
+
+        public void CanRemoveFirstFoundElement(int element, int indexOfElement)
+        {
+            var testArray = new IntArray();
+
+            testArray.Add(5);
+            testArray.Add(4);
+            testArray.Add(10);
+            testArray.Add(5);
+            testArray.Add(4);
+            testArray.Add(10);
+
+            testArray.Remove(element);
+
+            Assert.Equal(indexOfElement, testArray.IndexOf(element));
+        }
+
+        [Theory]
+        [InlineData(1, -1)]
+        [InlineData(2, -1)]
+        [InlineData(3, -1)]
+        [InlineData(4, -1)]
+        [InlineData(5, -1)]
+        [InlineData(6, -1)]
+
+        public void CanRemoveElementAtGivenIndex(int element, int indexOfElement)
+        {
+            var testArray = new IntArray();
+
+            testArray.Add(1);
+            testArray.Add(2);
+            testArray.Add(3);
+            testArray.Add(4);
+            testArray.Add(5);
+            testArray.Add(6);
+
+            testArray.Remove(element);
+
+            Assert.Equal(indexOfElement, testArray.IndexOf(element));
+        }
+    }
+}
---
+++ DataCollections/DataCollections/IntArray.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace DataCollections
+{
+    public class IntArray
+    {
+        private int[] array;
+
+        public IntArray()
+        {
+            array = new int[0];
+        }
+
+        public void Add(int element)
+        {
+            Array.Resize(ref array, array.Length + 1);
+            array[array.Length - 1] = element;
+        }
+
+        public int Count()
+        {
+            return array.Length;
+        }
+
+        public int Element(int index)
+        {
+            return array[index];
+        }
+
+        public void SetElement(int index, int element)
+        {
+            array[index] = element;
+        }
+
+        public bool Contains(int element)
+        {
+            return Array.Exists(array, elementToCheck => elementToCheck == element);
+        }
+
+        public int IndexOf(int element)
+        {
+            return Array.IndexOf(array, element);
+        }
+
+        public void Insert(int index, int element)
+        {
+            Array.Resize(ref array, array.Length + 1);
+            int oldArrayLength = array.Length - 1;
+            Array.Copy(array[..oldArrayLength], 0, array, 1, oldArrayLength);
+            array[index] = element;
+        }
+
+        public void Clear()
+        {
+            array = Array.Empty<int>();
+        }
+
+        public void Remove(int element)
+        {
+            int index = Array.IndexOf(array, element);
+            this.RemoveAt(index);
+        }
+
+        public void RemoveAt(int index)
+        {
+            Array.Copy(array, 0, array, 0, index);
+            Array.Copy(array, index + 1, array, index, array.Length - (index + 1));
+            Array.Resize(ref array, array.Length - 1);
+        }
+    }
+}
