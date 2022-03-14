55919594560520c7cb7db29dc344363cf88f3323
Cinciulescu Marian-Madalin <75886041+TheHawk3r@users.noreply.github.com>
2022-03-14 14:04:22 +0200

    Added Dictionary.cs

---
+++ DataCollections/DataCollections/Dictionary.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataCollections
+{
+    internal class Dictionary<TKey, TValue> : IDictionary<TKey, TValue>
+    {
+    }
+}
